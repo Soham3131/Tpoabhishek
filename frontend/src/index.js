83575dfc15d3e4d789ba5ee7d5ca3679bb6501f0
Soham3131 <sohamdang0@gmail.com>
2025-06-11 18:39:35 +0530

    Moved index.js to src folder

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./src/App";
+import { AuthProvider } from "./src/context/AuthContext";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
